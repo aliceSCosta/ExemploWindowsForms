f0a999aa6d06557e8a6458577ccff0cdef7db958
alicesilva <alicealmeyda72@gmail.com>
2019-06-11 16:59:36 -0300

    Adaptação do projeto para trablhar com classes (Pessoa), List e DataGridView

--- RegistroDePessoas/RegistroDePessoas/Form1.Designer.cs
+++ RegistroDePessoas/RegistroDePessoas/Form1.Designer.cs
@@ -31,13 +31,23 @@
             this.label1 = new System.Windows.Forms.Label();
             this.txtNome = new System.Windows.Forms.TextBox();
             this.btnEnviar = new System.Windows.Forms.Button();
-            this.txtCadastro = new System.Windows.Forms.TextBox();
+            this.nudIdade = new System.Windows.Forms.NumericUpDown();
+            this.label = new System.Windows.Forms.Label();
+            this.mskTelefone = new System.Windows.Forms.MaskedTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdoMasculino = new System.Windows.Forms.RadioButton();
+            this.rdoFeminino = new System.Windows.Forms.RadioButton();
+            this.dgvRegistros = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.nudIdade)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRegistros)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(30, 39);
+            this.label1.Location = new System.Drawing.Point(33, 36);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(38, 13);
             this.label1.TabIndex = 0;
@@ -47,12 +57,12 @@
             // 
             this.txtNome.Location = new System.Drawing.Point(74, 36);
             this.txtNome.Name = "txtNome";
-            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.Size = new System.Drawing.Size(120, 20);
             this.txtNome.TabIndex = 1;
             // 
             // btnEnviar
             // 
-            this.btnEnviar.Location = new System.Drawing.Point(180, 36);
+            this.btnEnviar.Location = new System.Drawing.Point(180, 228);
             this.btnEnviar.Name = "btnEnviar";
             this.btnEnviar.Size = new System.Drawing.Size(75, 23);
             this.btnEnviar.TabIndex = 2;
@@ -60,26 +70,111 @@
             this.btnEnviar.UseVisualStyleBackColor = true;
             this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
             // 
-            // txtCadastro
+            // nudIdade
             // 
-            this.txtCadastro.Location = new System.Drawing.Point(33, 75);
-            this.txtCadastro.Multiline = true;
-            this.txtCadastro.Name = "txtCadastro";
-            this.txtCadastro.ReadOnly = true;
-            this.txtCadastro.Size = new System.Drawing.Size(222, 338);
-            this.txtCadastro.TabIndex = 3;
+            this.nudIdade.Location = new System.Drawing.Point(74, 75);
+            this.nudIdade.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudIdade.Name = "nudIdade";
+            this.nudIdade.Size = new System.Drawing.Size(120, 20);
+            this.nudIdade.TabIndex = 4;
+            this.nudIdade.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(33, 75);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(37, 13);
+            this.label.TabIndex = 5;
+            this.label.Text = "Idade:";
+            // 
+            // mskTelefone
+            // 
+            this.mskTelefone.Location = new System.Drawing.Point(74, 110);
+            this.mskTelefone.Mask = "(99) 00000-0000";
+            this.mskTelefone.Name = "mskTelefone";
+            this.mskTelefone.Size = new System.Drawing.Size(120, 20);
+            this.mskTelefone.TabIndex = 6;
+            this.mskTelefone.TextMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Telefone:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdoMasculino);
+            this.groupBox1.Controls.Add(this.rdoFeminino);
+            this.groupBox1.Location = new System.Drawing.Point(19, 155);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(236, 67);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sexo";
+            // 
+            // rdoMasculino
+            // 
+            this.rdoMasculino.AutoSize = true;
+            this.rdoMasculino.Checked = true;
+            this.rdoMasculino.Location = new System.Drawing.Point(102, 33);
+            this.rdoMasculino.Name = "rdoMasculino";
+            this.rdoMasculino.Size = new System.Drawing.Size(73, 17);
+            this.rdoMasculino.TabIndex = 9;
+            this.rdoMasculino.TabStop = true;
+            this.rdoMasculino.Text = "Masculino";
+            this.rdoMasculino.UseVisualStyleBackColor = true;
+            // 
+            // rdoFeminino
+            // 
+            this.rdoFeminino.AutoSize = true;
+            this.rdoFeminino.Location = new System.Drawing.Point(14, 33);
+            this.rdoFeminino.Name = "rdoFeminino";
+            this.rdoFeminino.Size = new System.Drawing.Size(67, 17);
+            this.rdoFeminino.TabIndex = 9;
+            this.rdoFeminino.Text = "Feminino";
+            this.rdoFeminino.UseVisualStyleBackColor = true;
+            // 
+            // dgvRegistros
+            // 
+            this.dgvRegistros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRegistros.Location = new System.Drawing.Point(21, 257);
+            this.dgvRegistros.Name = "dgvRegistros";
+            this.dgvRegistros.Size = new System.Drawing.Size(240, 190);
+            this.dgvRegistros.TabIndex = 9;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 425);
-            this.Controls.Add(this.txtCadastro);
+            this.ClientSize = new System.Drawing.Size(292, 459);
+            this.Controls.Add(this.dgvRegistros);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.mskTelefone);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.nudIdade);
             this.Controls.Add(this.btnEnviar);
             this.Controls.Add(this.txtNome);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nudIdade)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRegistros)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -90,7 +185,14 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtNome;
         private System.Windows.Forms.Button btnEnviar;
-        private System.Windows.Forms.TextBox txtCadastro;
+        private System.Windows.Forms.NumericUpDown nudIdade;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.MaskedTextBox mskTelefone;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdoMasculino;
+        private System.Windows.Forms.RadioButton rdoFeminino;
+        private System.Windows.Forms.DataGridView dgvRegistros;
     }
 }
 
---
+++ RegistroDePessoas/RegistroDePessoas/Pessoa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroDePessoas
+{
+    class Pessoa
+    {
+        public String Nome { set; get; }
+        public Int32 Idade { set; get; }
+        public String Sexo { set; get; }
+        public Int64 Telefone { set; get; }
+
+
+        /*
+        public void ConfiguraNome(String valor)
+        {
+            this.nome = valor.ToUpper();
+        }
+
+        public String PegaNome()
+        {
+            return this.nome;
+        }
+        */
+    }
+}
